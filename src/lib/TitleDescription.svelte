a922eb22bddef4a09b39b1718a47e5f211da23cc
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-04-13 12:45:51 +0100

    Make a component dynamic

--- src/lib/TitleDescription.svelte
+++ src/lib/TitleDescription.svelte
@@ -1,6 +1,12 @@
+<script>
+    export let title = 'Title';
+    export let description = 'Description';
+</script>
 
 <h1 class="text-5xl font-extrabold dark:text-white">
-    Insert page Title
+    {title}
 </h1>
+
 <small class="ml-2 font-semibold text-gray-500 dark:text-gray-400">
-    This is page description</small>
+    {description}
+</small>
