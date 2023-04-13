5d8d13b639fbc164a2e9177c7b15f57a7eea8079
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-04-13 11:56:02 +0100

    Add Components from Tailwind

---
+++ src/lib/TitleDescription.svelte
@@ -0,0 +1,6 @@
+
+<h1 class="text-5xl font-extrabold dark:text-white">
+    Insert page Title
+</h1>
+<small class="ml-2 font-semibold text-gray-500 dark:text-gray-400">
+    This is page description</small>
