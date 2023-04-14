3da61eb1d06c710328b23d1f61a182b9ef62a29b
Danny Lewis <danny.lewis@peopleplus.co.uk>
2023-04-14 14:25:49 +0100

    Lint fixes

--- dist/TitleDescription.svelte
+++ dist/TitleDescription.svelte
@@ -1,12 +1,12 @@
 <script>
-    export let title = 'Title';
-    export let description = 'Description';
+	export let title = 'Title';
+	export let description = 'Description';
 </script>
 
 <h1 class="text-5xl font-extrabold dark:text-white">
-    {title}
+	{title}
 </h1>
 
 <small class="ml-2 font-semibold text-gray-500 dark:text-gray-400">
-    {description}
+	{description}
 </small>
--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,15 @@
 declare namespace App {
-    interface Locals {}
+	interface Locals {}
 
-   interface Platform {
-       env: {};
-       context: {
-           waitUntil(promise: Promise<any>): void;
-       };
-       caches: CacheStorage & { default: Cache }
-   }
+	interface Platform {
+		env: {};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		};
+		caches: CacheStorage & { default: Cache };
+	}
 
-    interface Session {}
+	interface Session {}
 
-    interface Stuff {}
+	interface Stuff {}
 }
