83fa94fb294c9120edebe851d77bca7c217dc1fc
Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>
2023-04-27 09:15:29 +0100

    Headers (#71)
    * Bump tailwindcss from 3.3.1 to 3.3.2 (#65)
    
    Bumps [tailwindcss](https://github.com/tailwindlabs/tailwindcss) from 3.3.1 to 3.3.2.
    - [Release notes](https://github.com/tailwindlabs/tailwindcss/releases)
    - [Changelog](https://github.com/tailwindlabs/tailwindcss/blob/v3.3.2/CHANGELOG.md)
    - [Commits](https://github.com/tailwindlabs/tailwindcss/compare/v3.3.1...v3.3.2)
    
    ---
    updated-dependencies:
    - dependency-name: tailwindcss
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump @playwright/test from 1.32.3 to 1.33.0 (#66)
    
    Bumps [@playwright/test](https://github.com/Microsoft/playwright) from 1.32.3 to 1.33.0.
    - [Release notes](https://github.com/Microsoft/playwright/releases)
    - [Commits](https://github.com/Microsoft/playwright/compare/v1.32.3...v1.33.0)
    
    ---
    updated-dependencies:
    - dependency-name: "@playwright/test"
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump @sveltejs/kit from 1.15.8 to 1.15.9 (#67)
    
    Bumps [@sveltejs/kit](https://github.com/sveltejs/kit/tree/HEAD/packages/kit) from 1.15.8 to 1.15.9.
    - [Release notes](https://github.com/sveltejs/kit/releases)
    - [Changelog](https://github.com/sveltejs/kit/blob/master/packages/kit/CHANGELOG.md)
    - [Commits](https://github.com/sveltejs/kit/commits/@sveltejs/kit@1.15.9/packages/kit)
    
    ---
    updated-dependencies:
    - dependency-name: "@sveltejs/kit"
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump vite from 4.3.1 to 4.3.3 (#68)
    
    Bumps [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite) from 4.3.1 to 4.3.3.
    - [Release notes](https://github.com/vitejs/vite/releases)
    - [Changelog](https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md)
    - [Commits](https://github.com/vitejs/vite/commits/v4.3.3/packages/vite)
    
    ---
    updated-dependencies:
    - dependency-name: vite
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Added some headers
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		csp: {
+			directives: {
+				'script-src': ['self'],
+				'style-src': ['self']
+			}
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
