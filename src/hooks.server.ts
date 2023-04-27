7eb72e5c5c1cc9b12f6ec85e8a8000e2f52b88d4
Danny Lewis <danny.lewis@peopleplus.co.uk>
2023-04-27 09:11:41 +0100

    Added some headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,13 @@ import { sequence } from '@sveltejs/kit/hooks';
 const headers = async function handle({ event, resolve }) {
 	const response = await resolve(event);
 	response.headers.set('X-Clacks-Overhead', 'GNU Terry Pratchett');
+	response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+	response.headers.set('X-Content-Type-Options', 'nosniff');
+	response.headers.set('X-Frame-Options', 'DENY');
+	response.headers.set('Permissions-Policy', '*');
+	response.headers.set('Cross-Origin-Resource-Policy', 'same-origin');
+	response.headers.set('Access-Control-Allow-Origin', '*');
+	response.headers.set('Access-Control-Allow-Credentials', 'true');
 	return response;
 };
 
