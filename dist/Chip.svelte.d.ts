9006737ba5f4bc55ee0fa99b96150198e33778f2
Danny Lewis <danny.lewis@peopleplus.co.uk>
2023-04-28 15:43:40 +0100

    Lint

--- dist/Chip.svelte.d.ts
+++ dist/Chip.svelte.d.ts
@@ -1,23 +1,26 @@
 /** @typedef {typeof __propDef.props}  ChipProps */
 /** @typedef {typeof __propDef.events}  ChipEvents */
 /** @typedef {typeof __propDef.slots}  ChipSlots */
-export default class Chip extends SvelteComponentTyped<{
-    [x: string]: never;
-}, {
-    [evt: string]: CustomEvent<any>;
-}, {}> {
-}
+export default class Chip extends SvelteComponentTyped<
+	{
+		[x: string]: never;
+	},
+	{
+		[evt: string]: CustomEvent<any>;
+	},
+	{}
+> {}
 export type ChipProps = typeof __propDef.props;
 export type ChipEvents = typeof __propDef.events;
 export type ChipSlots = typeof __propDef.slots;
-import { SvelteComponentTyped } from "svelte";
+import { SvelteComponentTyped } from 'svelte';
 declare const __propDef: {
-    props: {
-        [x: string]: never;
-    };
-    events: {
-        [evt: string]: CustomEvent<any>;
-    };
-    slots: {};
+	props: {
+		[x: string]: never;
+	};
+	events: {
+		[evt: string]: CustomEvent<any>;
+	};
+	slots: {};
 };
 export {};
--- dist/PrimaryButton.svelte.d.ts
+++ dist/PrimaryButton.svelte.d.ts
@@ -1,23 +1,26 @@
 /** @typedef {typeof __propDef.props}  PrimaryButtonProps */
 /** @typedef {typeof __propDef.events}  PrimaryButtonEvents */
 /** @typedef {typeof __propDef.slots}  PrimaryButtonSlots */
-export default class PrimaryButton extends SvelteComponentTyped<{
-    [x: string]: never;
-}, {
-    [evt: string]: CustomEvent<any>;
-}, {}> {
-}
+export default class PrimaryButton extends SvelteComponentTyped<
+	{
+		[x: string]: never;
+	},
+	{
+		[evt: string]: CustomEvent<any>;
+	},
+	{}
+> {}
 export type PrimaryButtonProps = typeof __propDef.props;
 export type PrimaryButtonEvents = typeof __propDef.events;
 export type PrimaryButtonSlots = typeof __propDef.slots;
-import { SvelteComponentTyped } from "svelte";
+import { SvelteComponentTyped } from 'svelte';
 declare const __propDef: {
-    props: {
-        [x: string]: never;
-    };
-    events: {
-        [evt: string]: CustomEvent<any>;
-    };
-    slots: {};
+	props: {
+		[x: string]: never;
+	};
+	events: {
+		[evt: string]: CustomEvent<any>;
+	};
+	slots: {};
 };
 export {};
