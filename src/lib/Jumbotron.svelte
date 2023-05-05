66745f9000c7c639ab6db9e1af4a17059d0ad23a
Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>
2023-05-05 17:04:19 +0100

    Jumbotron (#107)
    * add stepper component with tests (#103)
    
    * hook up props for the jumbotron (#106)
    
    * hook up props for the jumbotron
    
    * add slot to the jumbotron
    
    * add class prop passthrough to Jumbotron and remove default rounded borders
    
    * add `start` slot before the h1
    
    * add a `tight` modifier prop to reduce padding top & bottom
    
    ---------
    
    Co-authored-by: James Birtles <james@birtles.dev>

--- src/lib/Jumbotron.svelte
+++ src/lib/Jumbotron.svelte
@@ -1,41 +1,65 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	let className = '';
+	export { className as class };
+	export let title: string;
+	export let subtitle: string | undefined = undefined;
+	export let primaryAction: string | undefined = undefined;
+	export let primaryActionHref: string | undefined = undefined;
+	export let secondaryAction: string | undefined = undefined;
+	export let secondaryActionHref: string | undefined = undefined;
+	export let image: string | undefined = undefined;
+	/**
+	 * Use tighter padding top and bottom
+	 */
+	export let tight = false;
+
+	const dispatch = createEventDispatcher();
+</script>
+
 <section
-	class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply"
+	data-testid="jumbotron-root"
+	class="bg-cover bg-center bg-no-repeat bg-gray-700 bg-blend-multiply {className}"
+	style:background-image={image && `url(${JSON.stringify(image)})`}
 >
-	<div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+	<div
+		class="px-4 mx-auto max-w-screen-xl text-center {tight ? 'py-16 lg:py-24' : 'py-24 lg:py-56'}"
+	>
+		<slot name="start" />
+
 		<h1
 			class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl"
 		>
-			We invest in the world’s potential
+			{title}
 		</h1>
-		<p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-			We focus on markets where technology, innovation, and capital can unlock long-term value and
-			drive economic growth.
-		</p>
+		{#if subtitle}
+			<p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+				{subtitle}
+			</p>
+		{/if}
 		<div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-			<a
-				href="#"
-				class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-			>
-				Get started
-				<svg
-					aria-hidden="true"
-					class="ml-2 -mr-1 w-4 h-4"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					/></svg
+			{#if primaryAction}
+				<svelte:element
+					this={primaryActionHref ? 'a' : 'button'}
+					href={primaryActionHref}
+					class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+					on:click={() => dispatch('primaryAction')}
 				>
-			</a>
-			<a
-				href="#"
-				class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
-			>
-				Learn more
-			</a>
+					{primaryAction}
+				</svelte:element>
+			{/if}
+			{#if secondaryAction}
+				<svelte:element
+					this={secondaryActionHref ? 'a' : 'button'}
+					href={secondaryActionHref}
+					class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
+					on:click={() => dispatch('secondaryAction')}
+				>
+					{secondaryAction}
+				</svelte:element>
+			{/if}
 		</div>
+		<slot />
 	</div>
 </section>
