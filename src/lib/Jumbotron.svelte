9a291c5c8e8cf3e5b27e2e0707be981d271b7aaa
James Birtles <james@birtles.dev>
2023-05-05 13:14:23 +0100

    add `start` slot before the h1

--- src/lib/Jumbotron.svelte
+++ src/lib/Jumbotron.svelte
@@ -20,6 +20,8 @@
 	style:background-image={image && `url(${JSON.stringify(image)})`}
 >
 	<div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+		<slot name="start" />
+
 		<h1
 			class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl"
 		>
