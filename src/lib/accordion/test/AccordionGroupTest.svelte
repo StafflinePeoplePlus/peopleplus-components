004670a0a0c0692dd299aefdd4ae113483db0c6f
James Birtles <james@birtles.dev>
2023-05-09 11:24:57 +0100

    Accordion (#115)
    * Implement accordion & accordion group
    
    * use svelte-headlessui in accordion implementation

---
+++ src/lib/accordion/test/AccordionGroupTest.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Accordion, AccordionGroup } from '$lib';
+
+	export let exclusive = false;
+</script>
+
+<AccordionGroup {exclusive}>
+	<Accordion label="Accordion 1">Content 1</Accordion>
+	<Accordion label="Accordion 2">Content 2</Accordion>
+	<Accordion label="Accordion 3">Content 3</Accordion>
+</AccordionGroup>
