3aeabcc7689549a8f84ec76f67ccc0b5690a81c9
James Birtles <james@birtles.dev>
2023-05-09 10:39:08 +0100

    use svelte-headlessui in accordion implementation

--- src/lib/accordion/Accordion.svelte
+++ src/lib/accordion/Accordion.svelte
@@ -1,56 +1,61 @@
 <script lang="ts">
 	import { classes } from '$lib/classes';
-	import { createAccordion } from './state';
+	import { createDisclosure } from 'svelte-headlessui';
+	import { getAccordionGroup } from './AccordionGroup.svelte';
 
-	/**
-	 * ID used to hook up aria labels, it is advisable to set this when more than one item may appear on the page at a time.
-	 * When in a group favour setting it on the group instead
-	 */
-	export let id: string | undefined = undefined;
+	let className = '';
+	export { className as class };
 	export let label: string;
 
-	const accordion = createAccordion(id);
+	const group = getAccordionGroup();
+	const disclosure = createDisclosure({ label });
+	group?.registerDisclosure(disclosure);
+	$: disclosure.set({ label });
+
+	$: if (group && $disclosure.expanded) {
+		group.onDisclosureExpanded(disclosure);
+	}
 </script>
 
 <div
-	id={$accordion.id}
-	class="border-gray-200 bg-white {$accordion.inGroup
-		? 'group border-x border-t first:rounded-t-xl last:rounded-b-xl last:border-b'
-		: 'rounded-xl border'}"
+	class={classes(
+		'border-gray-200 bg-white',
+		group != null
+			? 'group border-x border-t first:rounded-t-xl last:rounded-b-xl last:border-b'
+			: 'rounded-xl border',
+		className
+	)}
 >
-	<h2 id="{$accordion.id}-heading">
+	<h2>
 		<button
 			type="button"
 			class={classes(
 				'flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 focus:outline-none focus:ring-2 focus:ring-red-400 focus:z-10 relative hover:bg-gray-50',
-				$accordion.inGroup ? 'group-first:rounded-t-xl' : 'rounded-t-xl',
-				!$accordion.expanded && ($accordion.inGroup ? 'group-last:rounded-b-xl' : 'rounded-b-xl')
+				group != null ? 'group-first:rounded-t-xl' : 'rounded-t-xl',
+				!$disclosure.expanded && (group != null ? 'group-last:rounded-b-xl' : 'rounded-b-xl')
 			)}
-			aria-expanded={$accordion.expanded}
-			aria-controls="{$accordion.id}-body"
-			on:click={() => accordion.toggle()}
+			use:disclosure.button
 		>
 			<span>{label}</span>
 			<svg
-				data-accordion-icon
 				class="w-6 h-6 shrink-0 transition"
-				class:rotate-180={$accordion.expanded}
+				class:rotate-180={$disclosure.expanded}
 				fill="currentColor"
 				viewBox="0 0 20 20"
 				xmlns="http://www.w3.org/2000/svg"
-				><path
+			>
+				<path
 					fill-rule="evenodd"
 					d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
 					clip-rule="evenodd"
-				/></svg
-			>
+				/>
+			</svg>
 		</button>
 	</h2>
 	<div
-		id="{$accordion.id}-body"
-		class:hidden={!$accordion.expanded}
+		class:hidden={!$disclosure.expanded}
 		class="p-5 border-t border-gray-200"
-		aria-labelledby="{$accordion.id}-heading"
+		use:disclosure.panel
 	>
 		<slot />
 	</div>
--- src/lib/accordion/AccordionGroup.svelte
+++ src/lib/accordion/AccordionGroup.svelte
@@ -1,16 +1,55 @@
-<script lang="ts">
-	import { createAccordionGroup } from './state';
+<script lang="ts" context="module">
+	import type { createDisclosure } from 'svelte-headlessui';
+	import { getContext, onDestroy } from 'svelte';
 
-	export let id: string | undefined = undefined;
+	const ctxKey = Symbol('Accordion Group');
+	type DisclosureStore = ReturnType<typeof createDisclosure>;
+	export type AccordionGroupContext = {
+		disclosures: DisclosureStore[];
+		onDisclosureExpanded(disclosure: DisclosureStore): void;
+		registerDisclosure(disclosure: DisclosureStore): void;
+	};
+
+	export function getAccordionGroup(): AccordionGroupContext | undefined {
+		return getContext(ctxKey);
+	}
+</script>
+
+<script lang="ts">
+	import { setContext } from 'svelte';
+
+	let className = '';
+	export { className as class };
 	/**
 	 * Only allow one accordion in the group to be open at a time
 	 */
 	export let exclusive = false;
 
-	const accordion = createAccordionGroup(id, { exclusive });
-	$: $accordion.options.exclusive = exclusive;
+	setContext<AccordionGroupContext>(ctxKey, {
+		disclosures: [],
+		onDisclosureExpanded(disclosure) {
+			if (exclusive) {
+				for (const disc of this.disclosures) {
+					if (disc === disclosure) {
+						continue;
+					}
+
+					disc.close();
+				}
+			}
+		},
+		registerDisclosure(disclosure) {
+			this.disclosures.push(disclosure);
+			onDestroy(() => {
+				const index = this.disclosures.findIndex((d) => d === disclosure);
+				if (index != -1) {
+					this.disclosures.splice(index, 1);
+				}
+			});
+		}
+	});
 </script>
 
-<div>
+<div class={className}>
 	<slot />
 </div>
