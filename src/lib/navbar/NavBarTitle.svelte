1dceda6195fe7473c2a4335119c5c054dea6c2d5
Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>
2023-05-09 17:00:19 +0100

    NavBar (#136)
    * Navbar (#134)
    
    * add configurable navbar component
    
    * add basic navbar test
    
    ---------
    
    Signed-off-by: Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>
    Co-authored-by: James Birtles <james@birtles.dev>

---
+++ src/lib/navbar/NavBarTitle.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let href: string | undefined = undefined;
+</script>
+
+<svelte:element this={href ? 'a' : 'div'} {href} class="flex items-center">
+	<slot />
+</svelte:element>
