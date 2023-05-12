af930da4affda15d43760ad468fc048d1ca96471
Arun Purewal <arun.purewal@peopleplus.co.uk>
2023-05-12 15:17:50 +0100

    Adjusted naming for slots

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -5,11 +5,11 @@
 
 <footer class="bg-white rounded-lg shadow dark:bg-gray-800 {className}">
 	<div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-		<slot name="start" />
+		<slot name="copyright" />
 		<ul
 			class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
 		>
-			<slot name="end" />
+			<slot name="links" />
 		</ul>
 	</div>
 </footer>
--- src/routes/Footer/ExampleFooter.svelte
+++ src/routes/Footer/ExampleFooter.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <Footer>
-	<Copyright slot="start">
+	<Copyright slot="copyright">
 		&copy; 2023
 		<a href="https://peopleplus.co.uk/" class="hover:underline"> PeoplePlus&trade; </a>. All Rights
 		Reserved.
 	</Copyright>
 
-	<svelte:fragment slot="end">
+	<svelte:fragment slot="links">
 		<ListItem href="/">About</ListItem>
 		<ListItem href="/">Privacy Policy</ListItem>
 		<ListItem href="/">Licensing</ListItem>
