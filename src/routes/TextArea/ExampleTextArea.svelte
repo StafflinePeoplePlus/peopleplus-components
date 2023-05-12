fca1c0d0945b276e956eb4a836450c533f1bf7db
Arun Purewal <132344003+ArunPurewal1994@users.noreply.github.com>
2023-05-12 15:07:57 +0100

    Text Input (#172)
    * Added Text Input Component - no validation
    
    * Minor fixes to Input Component
    
    * Seperated the label to it's own component
    
    * Added Text Area as it's own component
    
    * Added Input Error as it's own component
    
    * Clean up Input Components
    
    * Added class passthrough to components
    
    * Amended the class passthrough
    
    * Minor change to class spacing
    
    * Update src/lib/InputLabel.svelte
    
    Co-authored-by: James Birtles <james@birtles.dev>
    Signed-off-by: Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>
    Co-authored-by: Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>
    Co-authored-by: James Birtles <james@birtles.dev>

---
+++ src/routes/TextArea/ExampleTextArea.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { TextArea, InputLabel, InputError } from '$lib';
+</script>
+
+<div class="space-y-3">
+	<InputLabel for="example">Label</InputLabel>
+	<TextArea name="example" id="example" rows={4} />
+	<InputError>* This is an error...</InputError>
+</div>
---
+++ src/routes/TextInput/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import ExamplePage from '../ExamplePage.svelte';
+	import ExampleTextInput from './ExampleTextInput.svelte';
+	import usage from './ExampleTextInput.svelte?raw';
+</script>
+
+<ExamplePage title="TextInput" {usage}>
+	<ExampleTextInput />
+</ExamplePage>
