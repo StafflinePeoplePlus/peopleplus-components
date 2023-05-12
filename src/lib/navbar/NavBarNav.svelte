121e766ea22d30a2e5f30b11da080cf16f0cb8f6
James Birtles <james@birtles.dev>
2023-05-12 14:46:10 +0100

    add ability to set class on NavBarNav

--- src/lib/navbar/NavBarNav.svelte
+++ src/lib/navbar/NavBarNav.svelte
@@ -1,3 +1,8 @@
-<ul class="flex flex-col font-medium md:flex-row md:items-center md:gap-x-4 md:mt-0">
+<script lang="ts">
+	let className = '';
+	export { className as class };
+</script>
+
+<ul class="flex flex-col font-medium md:flex-row md:items-center md:gap-x-4 md:mt-0 {className}">
 	<slot />
 </ul>
