37e88803818b74c1ce0f2b099ee3ec13f8c97ef5
James Birtles <james@birtles.dev>
2023-05-12 14:57:09 +0100

    reorganise navbar playwright tests to test by screen

--- playwright_tests/NavBar.spec.ts
+++ playwright_tests/NavBar.spec.ts
@@ -1,20 +1,22 @@
 import { expect, test } from '@playwright/test';
 
-test('NavBar sections are visible', async ({ page }) => {
+test('NavBar works on larger screens', async ({ page }) => {
 	await page.goto('/test/navbar');
 	const navbar = page.getByTestId('navbar');
+	const hamburger = navbar.getByRole('button');
 
+	await expect(hamburger).not.toBeVisible();
 	await expect(navbar.getByTestId('start')).toBeVisible();
 	await expect(navbar.getByTestId('middle')).toBeVisible();
 	await expect(navbar.getByTestId('end')).toBeVisible();
 });
 
-test('NavBar hamburger on small screens', async ({ page }) => {
+test('NavBar works on mobile screens', async ({ page }) => {
+	await page.setViewportSize({ width: 767, height: 1000 });
 	await page.goto('/test/navbar');
 	const navbar = page.getByTestId('navbar');
 	const hamburger = navbar.getByRole('button');
-	await expect(hamburger).not.toBeVisible();
-	await page.setViewportSize({ width: 767, height: 1000 });
+
 	await expect(hamburger).toBeVisible();
 	await expect(navbar.getByTestId('start')).toBeVisible();
 	await expect(navbar.getByTestId('middle')).not.toBeVisible();
