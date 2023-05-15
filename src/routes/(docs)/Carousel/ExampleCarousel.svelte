e0b9dcd5c7e5a65b0fb7bc99fb2e29340ac7e159
James Birtles <James.Birtles@peopleplus.co.uk>
2023-05-15 17:06:32 +0100

    simplify styling on the example carousel

--- src/routes/(docs)/Carousel/ExampleCarousel.svelte
+++ src/routes/(docs)/Carousel/ExampleCarousel.svelte
@@ -4,6 +4,6 @@
 	const items = Array.from({ length: 10 }).map((_, i) => `/carousel/${i}.jpg`);
 </script>
 
-<Carousel {items} let:item class="!-mx-6 !px-6" itemClass="!w-[200px] mr-2">
-	<img src={item} width="400" height="400" alt="" class="rounded" />
+<Carousel {items} let:item class="-mx-6 px-6 gap-2">
+	<img src={item} width="400" height="400" alt="" class="rounded w-[200px]" />
 </Carousel>
