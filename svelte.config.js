f88d945c97ffc1eaa6c3c38e7a20976f361ad091
James Birtles <james@birtles.dev>
2023-05-18 11:09:02 +0100

    Refactor github actions, add changelog generation, version tagging (#249)
    * separate out actions into multiple jobs
    
    * don't use a matrix for zap and a11y scans
    
    * install playwright for a11y tests
    
    * try upgrade
    
    * add headers to static assets in preview server, add permissions-policy header
    
    * combine uat and dev owasp rules
    
    * don't install playwright on a11ywatch job
    
    * fail action if zap fails
    
    * update uat actions to match dev
    
    * add changelog generation on tag push
    
    * tag version on publish
    
    * test coverage badge experiments
    
    * quotes? and try this for color
    
    * enable sourcemaps for coverage tests
    
    * zap only concurrency, handle multiline json
    
    * lets just try a json action
    
    * update readme and add all summaries
    
    * move coverage updating to main pushes only
    
    * remove adapter-auto
    
    * update readme and add contributing
    
    * only add coverage if env is enabled
    
    * fix policy header format
    
    * remove istanbul-badges-readme

--- svelte.config.js
+++ svelte.config.js
@@ -14,14 +14,9 @@ const config = {
 				// Not able to be strict-dynamic due to https://github.com/sveltejs/kit/issues/3558
 				'default-src': ['self'],
 				'object-src': ['none'],
-				// Needed to allow swiper carousel icons
-				'font-src': ['data: unsafe-inline'],
 				'base-uri': ['none']
 			}
 		},
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
