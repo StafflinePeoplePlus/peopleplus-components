37354624e7c4d69143241e1edea285620397195d
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-05-19 13:07:11 +0100

    Update Card Component

--- src/routes/(docs)/Card/+page.svelte
+++ src/routes/(docs)/Card/+page.svelte
@@ -2,9 +2,16 @@
 	import { Card } from '$lib';
 	import ExamplePage from '../ExamplePage.svelte';
 
-	const usage = '<Card />';
+	import usage from './ExampleCard.svelte?raw';
 </script>
 
 <ExamplePage title="Card" {usage}>
-	<Card />
+	<Card
+		img="/jumbotron.jpg"
+		imgHeight="200px"
+		action="Learn More"
+		actionHref="#card"
+		title="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+		subtitle=" Mauris auctor cursus nibh, vitae lacinia nunc consequat id. Integer luctus nunc ut lorem lobortis, eu sagittis nisi varius. Sed sit amet nulla at nunc egestas placerat. Sed aliquet, nisi id tincidunt tristique, enim metus vestibulum justo, vitae rutrum mi mi ac nisi."
+	/>
 </ExamplePage>
