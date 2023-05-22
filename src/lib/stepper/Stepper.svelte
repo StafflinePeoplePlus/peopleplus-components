6969c6083d690b2a2add7f7da284dc83402ffe6c
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-05-22 16:47:05 +0100

    Update Stepper.svelte

--- src/lib/stepper/Stepper.svelte
+++ src/lib/stepper/Stepper.svelte
@@ -22,30 +22,30 @@
 </script>
 
 <div
-	class="mb-5 rounded-lg border border-gray-200 bg-white font-medium shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 lg:flex lg:space-x-4 p-2"
+	class="mb-5 rounded-lg border border-gray-200 bg-white p-2 font-medium shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 lg:flex lg:space-x-4"
 >
 	{#each steps as step, index}
 		<button
-			class={`rounded-lg text-gray-500 md:w-1/2 p-2 sm:p-3   ${
+			class={`rounded-lg p-2 text-gray-500 sm:p-3 md:w-1/2   ${
 				step === activeStep ? 'text-primary-500' : ''
 			}`}
 			on:click={() => changeStep(step)}
 		>
 			<div>
-<span
-		class={`rounded-full bg-gray-500 py-1 px-2 text-xs text-white ${
-					step === activeStep ? 'bg-primary-500' : ''
-				}`}
->
-				{index + 1}
-			</span>
+				<span
+					class={`rounded-full bg-gray-500 px-2 py-1 text-xs text-white ${
+						step === activeStep ? 'bg-primary-500' : ''
+					}`}
+				>
+					{index + 1}
+				</span>
 				{step.label}
 			</div>
-
 		</button>
 		{#if index !== steps.length - 1}
-			<div class="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden md:after:inline-block dark:after:border-gray-700">
-			</div>
+			<div
+				class="after:border-1 flex items-center after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 after:content-[''] dark:after:border-gray-700 md:w-full md:after:inline-block"
+			/>
 		{/if}
 	{/each}
 </div>
