554cfa6c2389e6a2739833af30fa0a461a6e15c1
Anna Fatani <79071385+annafatani@users.noreply.github.com>
2023-05-26 13:33:37 +0100

    Update Stepper styling and test (#285)
    * Update Stepper styling and test
    
    * Update Stepper.svelte
    
    * Fix failed test
    
    * Update hover styling
    
    * Fix format
    
    * Update stepper styling
    
    * Resolve comments
    
    * Fixed #285- Update active button styling
    
    ---------
    
    Co-authored-by: Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>

--- src/lib/Stepper.svelte
+++ src/lib/Stepper.svelte
@@ -1,27 +1,60 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
-
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import { twMerge } from 'tailwind-merge';
 	export type Step = $$Generic<{ label: string }>;
+	type $$Props = SvelteHTMLElements['section'];
+
+	export { className as class };
+	let className: $$Props['class'] = undefined;
+
 	export let steps: Step[];
+	export let activeStep: Step | undefined = undefined;
+
+	$: if (steps && steps.length > 0) {
+		activeStep = steps[0];
+	}
 
 	interface $$Events {
 		changeStep: CustomEvent<Step>;
 	}
 	const dispatchEvent = createEventDispatcher();
+
+	function changeStep(step: Step) {
+		activeStep = step;
+		dispatchEvent('changeStep', step);
+	}
 </script>
 
-<div
-	class="flex w-full flex-wrap items-center space-x-2 rounded-lg border border-gray-200 bg-white p-3 text-sm font-medium text-gray-500 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 sm:space-x-4 sm:p-4 sm:text-base"
+<section
+	class={twMerge(
+		'mb-5 rounded-lg border border-gray-200 bg-white p-2 font-medium shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 lg:flex lg:px-0',
+		className
+	)}
+	{...$$restProps}
 >
 	{#each steps as step, index}
 		<button
-			on:click={() => dispatchEvent('changeStep', step)}
-			class="cursor-pointer hover:text-red-700"
+			class={`w-full rounded-lg p-2 text-left text-gray-500 lg:mx-2 lg:w-3/4 lg:text-center ${
+				step === activeStep ? 'bg-primary-500 text-white' : ''
+			}`}
+			on:click={() => changeStep(step)}
 		>
+			<span
+				class={`rounded-full bg-gray-500 px-2 py-1 text-xs text-white ${
+					step === activeStep ? 'border border-white bg-primary-500' : ''
+				}`}
+			>
+				{index + 1}
+			</span>
+			<br class="hidden lg:block" />
 			{step.label}
 		</button>
 		{#if index !== steps.length - 1}
-			<span class="ml-3 text-xl font-thin"> &raquo; </span>
+			<div
+				class="after:border-1 flex items-center after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 after:content-[''] dark:after:border-gray-700 md:w-full lg:after:inline-block"
+			/>
 		{/if}
 	{/each}
-</div>
+	<slot />
+</section>
