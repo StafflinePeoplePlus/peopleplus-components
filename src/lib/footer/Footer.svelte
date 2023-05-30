9b7ebb4d4c0f99e99968531ddf135e081f6d5fa9
Arun Purewal <arun.purewal@peopleplus.co.uk>
2023-05-30 11:44:39 +0100

    Responsive changes

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -4,10 +4,12 @@
 </script>
 
 <footer class="bg-white rounded-lg shadow dark:bg-gray-800 {className}">
-	<div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+	<div
+		class="w-full mx-auto max-w-screen-xl p-4 flex flex-col md:flex-row space-y-1 items-center md:justify-between"
+	>
 		<slot name="copyright" />
 		<ul
-			class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+			class="flex md:flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
 		>
 			<slot name="links" />
 		</ul>
