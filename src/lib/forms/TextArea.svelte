3bc13a9446ea55fe10f3fd74a55fc7e081f35241
James Birtles <james@birtles.dev>
2023-05-30 11:55:19 +0100

    add value binding and input/change events to inputs (#324)

--- src/lib/forms/TextArea.svelte
+++ src/lib/forms/TextArea.svelte
@@ -6,9 +6,13 @@
 
 	let className: $$Props['class'] = undefined;
 	export { className as class };
+	export let value: $$Props['value'] = undefined;
 </script>
 
 <textarea
 	{...$$restProps}
 	class={twMerge('block w-full bg-transparent py-2.5 focus:outline-none', className)}
+	bind:value
+	on:input
+	on:change
 />
--- src/lib/forms/TextInput.svelte
+++ src/lib/forms/TextInput.svelte
@@ -6,10 +6,14 @@
 
 	let className: $$Props['class'] = undefined;
 	export { className as class };
+	export let value: $$Props['value'] = undefined;
 </script>
 
 <input
 	{...$$restProps}
 	type="text"
 	class={twMerge('block w-full bg-transparent py-2.5 focus:outline-none', className)}
+	bind:value
+	on:input
+	on:change
 />
