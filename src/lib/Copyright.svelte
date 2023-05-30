1603777cd7f1c4d59a1b1ebb23ba49aefbb8d358
Arun Purewal <arun.purewal@peopleplus.co.uk>
2023-05-30 10:17:40 +0100

    Added missing classname prop

--- src/lib/Copyright.svelte
+++ src/lib/Copyright.svelte
@@ -1,3 +1,8 @@
-<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+<script lang="ts">
+	let className = '';
+	export { className as class };
+</script>
+
+<span class="{className} text-sm text-gray-500 sm:text-center dark:text-gray-400">
 	<slot />
 </span>
--- src/lib/ListItem.svelte
+++ src/lib/ListItem.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	export let href: string;
+	let className = '';
+	export { className as class };
 </script>
 
 <li>
-	<a {href} class="mr-4 hover:underline md:mr-6">
+	<a {href} class="{className} mr-4 hover:underline md:mr-6">
 		<slot />
 	</a>
 </li>
