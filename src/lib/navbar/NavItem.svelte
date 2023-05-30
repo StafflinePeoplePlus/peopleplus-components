3fc5f7af15944a791783834129e0989a4575320a
Anna Fatani <79071385+annafatani@users.noreply.github.com>
2023-05-30 11:57:41 +0100

    Fixed #330 Allow additional props in the NavItem (#331)

--- src/lib/navbar/NavItem.svelte
+++ src/lib/navbar/NavItem.svelte
@@ -14,6 +14,7 @@
 			'block border-b border-gray-200 py-2 pl-3 pr-4 text-left text-gray-900 transition hover:bg-gray-50 dark:border-white/20 dark:text-white dark:hover:bg-white/20 md:border-0 md:p-1 md:hover:bg-transparent md:hover:text-primary-700 dark:md:hover:bg-transparent dark:md:hover:text-primary-400',
 			className
 		)}
+		{...$$restProps}
 	>
 		<slot />
 	</svelte:element>
