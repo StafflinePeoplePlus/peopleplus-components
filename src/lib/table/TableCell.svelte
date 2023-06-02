4f633ce73fdb184d2574fdcd33f20191a2027199
James Birtles <James.Birtles@peopleplus.co.uk>
2023-06-02 12:20:22 +0100

    add Table component

---
+++ src/lib/table/TableCell.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import { twMerge } from 'tailwind-merge';
+	import { isPosition, type VisualPosition } from './position';
+	import { getTableContext } from './context';
+
+	type Props = {
+		visualPosition?: VisualPosition;
+		number?: boolean;
+	};
+	type $$Props =
+		| (Props & { header: true } & SvelteHTMLElements['th'])
+		| (Props & { header?: false } & SvelteHTMLElements['td']);
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+	export let header: $$Props['header'] = false;
+	export let number: $$Props['number'] = false;
+	/**
+	 * Override the visual position of the cell. In table layouts utilising row/colspan it can be
+	 * difficult to determine in css which cells are visually in the corners to apply the correct
+	 * border radius to.
+	 */
+	export let visualPosition: $$Props['visualPosition'] = undefined;
+
+	const table = getTableContext();
+</script>
+
+<svelte:element
+	this={header ? 'th' : 'td'}
+	class={twMerge(
+		'px-3 py-2',
+		header && 'bg-gray-200 font-semibold text-gray-800',
+		number && 'text-right',
+		$table.borders && 'ring-1 ring-gray-300',
+		visualPosition
+			? isPosition(visualPosition, 'top-left') && 'rounded-tl-md'
+			: 'group-first-of-type/tr:first:rounded-tl-md',
+		visualPosition
+			? isPosition(visualPosition, 'top-right') && 'rounded-tr-md'
+			: 'group-first-of-type/tr:last:rounded-tr-md',
+		visualPosition
+			? isPosition(visualPosition, 'bottom-left') && 'rounded-bl-md'
+			: 'group-last-of-type/tr:first:rounded-bl-md',
+		visualPosition
+			? isPosition(visualPosition, 'bottom-right') && 'rounded-br-md'
+			: 'group-last-of-type/tr:last:rounded-br-md',
+		className
+	)}
+	{...$$restProps}
+>
+	<slot />
+</svelte:element>
---
+++ src/lib/table/TableRow.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	import { twMerge } from 'tailwind-merge';
+	import { getTableContext } from './context';
+
+	type $$Props = SvelteHTMLElements['tr'];
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+
+	const table = getTableContext();
+</script>
+
+<tr
+	class={twMerge('group/tr', $table.striped && '[&:nth-of-type(2n+1)]:bg-gray-100', className)}
+	{...$$restProps}
+>
+	<slot />
+</tr>
