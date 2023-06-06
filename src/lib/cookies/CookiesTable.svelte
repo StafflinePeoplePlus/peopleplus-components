3d469554863a77459b2a8119cc70417a3ec99a6b
James Birtles <James.Birtles@peopleplus.co.uk>
2023-06-06 14:55:49 +0100

    scroll table on small width screens

--- src/lib/cookies/CookiesTable.svelte
+++ src/lib/cookies/CookiesTable.svelte
@@ -7,19 +7,21 @@
 	export let cookies: CookieDescription[];
 </script>
 
-<Table striped class={className}>
-	<TableRow>
-		<TableCell class="text-left" header>Name</TableCell>
-		<TableCell class="text-left" header>Provider</TableCell>
-		<TableCell class="text-left" header>Expiry</TableCell>
-		<TableCell class="text-left" header>Purpose</TableCell>
-	</TableRow>
-	{#each cookies as cookie}
+<div class="overflow-x-auto">
+	<Table striped class={className}>
 		<TableRow>
-			<TableCell>{cookie.name}</TableCell>
-			<TableCell>{cookie.provider}</TableCell>
-			<TableCell>{cookie.expiration}</TableCell>
-			<TableCell>{cookie.purpose}</TableCell>
+			<TableCell class="text-left" header>Name</TableCell>
+			<TableCell class="text-left" header>Provider</TableCell>
+			<TableCell class="text-left" header>Expiry</TableCell>
+			<TableCell class="text-left" header>Purpose</TableCell>
 		</TableRow>
-	{/each}
-</Table>
+		{#each cookies as cookie}
+			<TableRow>
+				<TableCell>{cookie.name}</TableCell>
+				<TableCell>{cookie.provider}</TableCell>
+				<TableCell>{cookie.expiration}</TableCell>
+				<TableCell>{cookie.purpose}</TableCell>
+			</TableRow>
+		{/each}
+	</Table>
+</div>
