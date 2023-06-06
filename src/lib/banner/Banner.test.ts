f6e0b2ed6916d56395dda78e0f5921622d64e2f1
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-06-06 14:41:37 +0100

    Readded TestBanner.svelte

--- src/lib/banner/Banner.test.ts
+++ src/lib/banner/Banner.test.ts
@@ -1,24 +1,28 @@
+import TestBanner from './TestBanner.svelte';
 import { test, expect, afterEach } from 'vitest';
-import { cleanup, fireEvent, render } from '@testing-library/svelte';
-import ExampleBanner from '../../routes/(docs)/Banner/ExampleBanner.svelte';
+import { cleanup, fireEvent, render, waitForElementToBeRemoved } from '@testing-library/svelte';
 
 afterEach(cleanup);
 
 test('should render everything', () => {
-	const { getByText } = render(ExampleBanner);
+	const { getByText } = render(TestBanner);
 
-	getByText(`'The Lyrics, The Music & The Money'`);
+	getByText('Banner Title');
+	getByText('Banner Description');
 
-	const primaryAction = getByText('View Course');
+	const primaryAction = getByText('Learn More');
 	expect(primaryAction.tagName).toBe('A');
 	expect(primaryAction.getAttribute('href')).toBe('https://peopleplus.co.uk');
 });
 
 test('should dismiss the banner once clicked', async () => {
-	const { getByText, queryByText } = render(ExampleBanner);
+	const { getByText, queryByText } = render(TestBanner);
+	getByText('Banner Title');
 
 	const dismissButton = getByText('Dismiss');
 	fireEvent.click(dismissButton);
 
-	expect(queryByText('The Lyrics, The Music & The Money')).to.not.exist;
+	await waitForElementToBeRemoved(() => queryByText('Banner Title'));
+
+	expect(queryByText('Banner Title')).to.not.exist;
 });
--- src/routes/test/banner/+page.svelte
+++ src/routes/test/banner/+page.svelte
@@ -1,26 +1,5 @@
 <script>
-	import Banner from '$lib/banner/Banner.svelte';
-	import { Button, Typography } from '$lib';
+	import ExampleBanner from '../../(docs)/Banner/ExampleBanner.svelte';
 </script>
 
-<Banner>
-	<div class="items-center gap-4 md:flex">
-		<div class="relative h-28 rounded-lg bg-cover bg-center md:h-40 md:w-80 lg:h-20 lg:w-64">
-			<img
-				src="/jumbotron.jpg"
-				alt=""
-				class="absolute inset-0 h-full w-full rounded-lg object-cover object-center"
-			/>
-		</div>
-		<div class="space-y-1 max-sm:pb-2">
-			<Typography variant="sub-heading">'The Lyrics, The Music & The Money'</Typography>
-			<Typography variant="body">
-				Introducing our latest course! Unravel the complex world of music industry economics and
-				understand the artistry behind creating impactful lyrics.
-			</Typography>
-		</div>
-		<Button href="https://peopleplus.co.uk" class="h-12 w-full md:w-1/2 lg:w-1/4"
-			>View Course</Button
-		>
-	</div>
-</Banner>
+<ExampleBanner />
