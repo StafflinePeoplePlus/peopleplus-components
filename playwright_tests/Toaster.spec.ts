b22f8c8f3b14a1bd834a3141ce5cf3e270c0e841
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-06-20 13:32:00 +0100

    Resolve comment

--- playwright_tests/Toaster.spec.ts
+++ playwright_tests/Toaster.spec.ts
@@ -6,23 +6,20 @@ const NULL_TIMEOUT_MESSAGE = 'Uploading videos.';
 test('toast should appear after a button click', async ({ page }) => {
 	await page.goto('/Toaster');
 	await page.getByRole('button', { name: 'Delete' }).click();
-	await expect(page.getByRole('alert', { name: `${TIMEOUT_MESSAGE}` })).toBeVisible();
+	await expect(page.getByRole('alert', { name: TIMEOUT_MESSAGE })).toBeVisible();
 });
 
 test('toast should disappear after a button click', async ({ page }) => {
 	await page.goto('/Toaster');
 	await page.getByRole('button', { name: 'Delete' }).click();
-	await expect(page.getByRole('alert', { name: `${TIMEOUT_MESSAGE}` })).not.toBeVisible();
+	await expect(page.getByRole('alert', { name: TIMEOUT_MESSAGE })).not.toBeVisible();
 });
 
 test('toast should not disappear if timeout is set to null', async ({ page }) => {
 	await page.goto('/Toaster');
 	await page.getByRole('button', { name: 'Upload' }).click();
-	await expect(page.getByRole('alert', { name: `${NULL_TIMEOUT_MESSAGE}` })).toBeVisible();
+	await expect(page.getByRole('alert', { name: NULL_TIMEOUT_MESSAGE })).toBeVisible();
 
-	await page
-		.getByRole('alert', { name: `${NULL_TIMEOUT_MESSAGE}` })
-		.getByRole('button')
-		.click();
-	await expect(page.getByRole('alert', { name: `${NULL_TIMEOUT_MESSAGE}` })).not.toBeVisible();
+	await page.getByRole('alert', { name: NULL_TIMEOUT_MESSAGE }).getByRole('button').click();
+	await expect(page.getByRole('alert', { name: NULL_TIMEOUT_MESSAGE })).not.toBeVisible();
 });
