1979fc0b311c6cc85def26ae34f6a50063cfd32c
James Birtles <James.Birtles@peopleplus.co.uk>
2023-06-20 10:18:52 +0100

    simplify example

--- src/routes/(docs)/Sortable/ExampleSortable.svelte
+++ src/routes/(docs)/Sortable/ExampleSortable.svelte
@@ -5,27 +5,10 @@
 	import { flip } from 'svelte/animate';
 	import { writable } from 'svelte/store';
 
-	let items = [
-		{ id: '1', name: 'Item 1' },
-		{ id: '2', name: 'Item 2' },
-		{ id: '3', name: 'Item 3' },
-		{ id: '4', name: 'Item 4' },
-		{ id: '5', name: 'Item 5' },
-		{ id: '6', name: 'Item 6' },
-		{ id: '7', name: 'Item 7' },
-		{ id: '8', name: 'Item 8' },
-		{ id: '9', name: 'Item 9' },
-		{ id: '10', name: 'Item 10' },
-		{ id: '11', name: 'Item 11' },
-		{ id: '12', name: 'Item 12' },
-		{ id: '13', name: 'Item 13' },
-		{ id: '14', name: 'Item 14' },
-		{ id: '15', name: 'Item 15' },
-		{ id: '16', name: 'Item 16' },
-		{ id: '17', name: 'Item 17' },
-		{ id: '18', name: 'Item 18' },
-		{ id: '19', name: 'Item 19' }
-	];
+	let items = Array.from({ length: 20 }).map((_, i) => ({
+		id: i.toString(),
+		name: `Item ${i + 1}`
+	}));
 	const sortingEnabled = writable(true);
 	const sortableX = createSortable({
 		axis: 'x',
