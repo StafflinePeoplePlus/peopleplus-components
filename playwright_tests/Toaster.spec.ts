47312c218b7ded831653fb5db6fd6a9ba1578f2d
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-06-20 12:40:13 +0100

    Update test

--- playwright_tests/Toaster.spec.ts
+++ playwright_tests/Toaster.spec.ts
@@ -1,25 +1,36 @@
 import { test, expect } from '@playwright/test';
 
+const TIMEOUT_MESSAGE = 'Changes successfully saved.';
+const NULL_TIMEOUT_MESSAGE = 'Uploading videos.';
+
+test('toast should appear after a button click', async ({ page }) => {
+	await page.goto('/Toaster');
+	await page.getByRole('button', { name: 'Delete' }).click();
+	await page.waitForTimeout(1000);
+	const isToastPresent = await page.$(`text="${TIMEOUT_MESSAGE}"`);
+	expect(isToastPresent).not.toBeNull();
+});
+
 test('toast should disappear after a button click', async ({ page }) => {
 	await page.goto('/Toaster');
 	await page.getByRole('button', { name: 'Delete' }).click();
 	await page.waitForTimeout(5000);
-	const isToastPresent = await page.$('text="Changes successfully saved."');
+	const isToastPresent = await page.$(`text="${TIMEOUT_MESSAGE}"`);
 	expect(isToastPresent).toBe(null);
 });
 
-test('toast should not disappear after a button click if timeout is set to null', async ({
+test('toast should not disappear timeout is set to null', async ({
 	page
 }) => {
 	await page.goto('/Toaster');
 	await page.getByRole('button', { name: 'Upload' }).click();
-	await page.getByText('text="Uploading videos."');
+	await page.getByText(`text="${NULL_TIMEOUT_MESSAGE}"`);
 	await page.waitForTimeout(6000);
-	const isToastPresent = await page.$('text="Uploading videos."');
+	const isToastPresent = await page.$(`text="${NULL_TIMEOUT_MESSAGE}"`);
 	expect(isToastPresent).not.toBeNull();
 
-	await page.getByRole('alert', { name: 'Uploading videos.' }).getByRole('button').click();
+	await page.getByRole('alert', { name: NULL_TIMEOUT_MESSAGE }).getByRole('button').click();
 	await page.waitForTimeout(1000);
-	const isToast = await page.$('text="Uploading videos."');
+	const isToast = await page.$(`text="${NULL_TIMEOUT_MESSAGE}"`);
 	expect(isToast).toBeNull();
 });
