827c68c87e8d55f98818ffadec8e15629ba17713
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-06-21 12:31:35 +0100

    fix format

--- src/routes/(docs)/Footer/ExampleFooter.svelte.d.ts
+++ src/routes/(docs)/Footer/ExampleFooter.svelte.d.ts
@@ -1,23 +1,26 @@
 /** @typedef {typeof __propDef.props}  ExampleFooterProps */
 /** @typedef {typeof __propDef.events}  ExampleFooterEvents */
 /** @typedef {typeof __propDef.slots}  ExampleFooterSlots */
-export default class ExampleFooter extends SvelteComponentTyped<{
-    [x: string]: never;
-}, {
-    [evt: string]: CustomEvent<any>;
-}, {}> {
-}
+export default class ExampleFooter extends SvelteComponentTyped<
+	{
+		[x: string]: never;
+	},
+	{
+		[evt: string]: CustomEvent<any>;
+	},
+	{}
+> {}
 export type ExampleFooterProps = typeof __propDef.props;
 export type ExampleFooterEvents = typeof __propDef.events;
 export type ExampleFooterSlots = typeof __propDef.slots;
-import { SvelteComponentTyped } from "svelte";
+import { SvelteComponentTyped } from 'svelte';
 declare const __propDef: {
-    props: {
-        [x: string]: never;
-    };
-    events: {
-        [evt: string]: CustomEvent<any>;
-    };
-    slots: {};
+	props: {
+		[x: string]: never;
+	};
+	events: {
+		[evt: string]: CustomEvent<any>;
+	};
+	slots: {};
 };
 export {};
