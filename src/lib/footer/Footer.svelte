e1fd7497cdfd4dc2786d3096058763cc9eabf5b4
Anna Fatani <79071385+annafatani@users.noreply.github.com>
2023-06-21 15:06:14 +0100

    Feat/make footer configurable (#475)
    * Make footer configurable
    
    * Add bottom slot
    
    * Add bottom slot
    
    * Add test
    
    * fix format
    
    * fix format
    
    * Replace Way2Learn with PeoplePlus
    
    * Remove twMerge
    
    * Readded twMerge
    
    * Remove twMerge from endClass

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	let className = '';
+	export { className as class };
+	export let startClass = '';
+	export let middleClass = '';
+	export let endClass = '';
+	export let bottomClass = '';
+</script>
+
+<div
+	class={twMerge(
+		'max-w-screen-2xl bg-white p-4 text-sm dark:bg-gray-700 sm:p-10 2xl:mx-auto',
+		className
+	)}
+	{...$$restProps}
+>
+	<div class={'justify-between lg:flex'}>
+		<div class={twMerge('flex w-full md:w-auto', startClass)}>
+			<slot name="start" />
+		</div>
+		<div class={'md:contents md:w-auto'}>
+			<div class={twMerge('max-lg:my-5', middleClass)}>
+				<slot name="middle" />
+			</div>
+			<div class={endClass}>
+				<slot name="end" />
+			</div>
+		</div>
+	</div>
+
+	<div class={twMerge('mt-5 border-t border-gray-100 pt-5', bottomClass)}>
+		<slot name="bottom" />
+	</div>
+</div>
