eba47761a47b6bdfc565ba207ace170710a60fa2
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-06-28 14:18:08 +0100

    Update test format

--- src/lib/breadcrumbs/BreadcrumbItem.svelte
+++ src/lib/breadcrumbs/BreadcrumbItem.svelte
@@ -2,11 +2,17 @@
 	export let isLast = false;
 </script>
 
-<div
-	class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-primary-600 dark:text-gray-400 dark:hover:text-white"
->
-	<slot />
-	{#if !isLast}
+{#if !isLast}
+	<div
+		class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-primary-600 dark:text-gray-400 dark:hover:text-white"
+	>
+		<slot />
 		<span class="pl-4 text-gray-400">&rsaquo;</span>
-	{/if}
-</div>
+	</div>
+{:else}
+	<div
+		class="inline-flex items-center text-sm font-medium text-gray-500 dark:text-gray-400 dark:hover:text-white"
+	>
+		<slot />
+	</div>
+{/if}
--- src/lib/breadcrumbs/Breadcrumbs.test.ts
+++ src/lib/breadcrumbs/Breadcrumbs.test.ts
@@ -6,21 +6,27 @@ import matchers from '@testing-library/jest-dom/matchers';
 expect.extend(matchers);
 afterEach(cleanup);
 
+const ITEM_LABEL_1 = 'Find Flights';
+const ITEM_LINK_1 = '#home';
+const ITEM_LABEL_2 = 'Canada';
+const ITEM_LINK_2 = '#canada';
+const LAST_ITEM = 'Vancouver';
+
 test('should render breadcrumb items', () => {
 	const { getByText } = render(Breadcrumbs);
 
-	const findFlights = getByText('Find Flights');
+	const findFlights = getByText(ITEM_LABEL_1);
 	expect(findFlights.tagName).toBe('A');
-	expect(findFlights.getAttribute('href')).toBe('#home');
+	expect(findFlights.getAttribute('href')).toBe(ITEM_LINK_1);
 
-	const canada = getByText('Canada');
+	const canada = getByText(ITEM_LABEL_2);
 	expect(canada.tagName).toBe('A');
-	expect(canada.getAttribute('href')).toBe('#canada');
+	expect(canada.getAttribute('href')).toBe(ITEM_LINK_2);
 });
 
 test('should not render an arrow after the last breadcrumb item', () => {
 	const { getByText } = render(Breadcrumbs);
-	const lastItem = getByText('Vancouver');
+	const lastItem = getByText(LAST_ITEM);
 	const arrow = within(lastItem).queryByText('>');
 	if (!arrow) {
 		return;
