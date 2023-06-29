b28f226a8239ba2bce68cb7cbe462f5fe1cc9bab
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-06-29 12:21:54 +0100

    Add Drawer sample

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -1,42 +1,35 @@
 <script lang="ts">
-	import type { SvelteHTMLElements } from 'svelte/elements';
-	import { twMerge } from 'tailwind-merge';
-	export let absolute = false;
+	import type {SvelteHTMLElements} from 'svelte/elements';
+	import {twMerge} from 'tailwind-merge';
+	import Button from '$lib/Button.svelte';
+
 	export let open = false;
 
 	type $$Props = SvelteHTMLElements['div'];
 
 	let className: $$Props['class'] = undefined;
-	export { className as class };
+	export {className as class};
 </script>
 
-<div class="bottom-0 right-0 top-0 z-40 {absolute ? 'absolute' : 'fixed'}" {...$$restProps}>
-	<div
-		id="drawer-right-example"
-		class={twMerge(
-		'h-full w-80 overflow-y-auto bg-white p-4 transition-transform dark:bg-gray-800',
+<div
+	id="drawer"
+	class:translate-x-full={!open}
+	tabindex="-1"
+	aria-labelledby="drawer-right-label"
+	class={twMerge(
+		'fixed bottom-0 right-0 top-0 z-40 h-full w-80 overflow-y-auto bg-white p-4 transition-transform dark:bg-gray-800 shadow-xl',
 		className
 	)}
-		class:translate-x-full={!open}
-		tabindex="-1"
-		aria-labelledby="drawer-right-label"
-	>
-		<h5
-			id="drawer-right-label"
-			class="mb-4 inline-flex items-center text-base font-semibold text-gray-500 dark:text-gray-400"
-		>
-			Right drawer
-		</h5>
-		<button
-			type="button"
-			aria-controls="drawer-right-example"
-			class="absolute right-2.5 top-2.5 inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+	{...$$restProps}
+>
+	<Button
+			aria-controls="drawer"
 			on:click={() => (open = false)}
-		>
-			x
-			<span class="sr-only">Close menu</span>
-		</button>
+			variant="secondary"
+			class="right-0 absolute"
+	>
+		&times;
+	</Button>
 
-		<slot />
-	</div>
+	<slot />
 </div>
--- src/routes/(docs)/Drawer/ExampleDrawer.svelte
+++ src/routes/(docs)/Drawer/ExampleDrawer.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
-	import { Button, Drawer } from '$lib';
-	let open = false;
+    import {Button, Drawer, Typography, Chip} from '$lib';
+
+    let open = false;
+
+    const columns = [
+        { label: 'Size', value: 'XS' },
+        { label: 'Units tested', value: '8' },
+        { label: 'Expected value', value: '16.00cm' },
+        { label: 'Tolerance', value: '-1.5cm / +1cm' },
+        { label: 'Status', value: 'Failed' }
+    ];
 </script>
 
 <Button on:click={() => (open = !open)}>View Measurements</Button>
 
-<Drawer fixed bind:open />
+<Drawer fixed bind:open>
+    <Typography variant="body-lg" class="font-bold">Measurements</Typography>
+    <Typography variant="body">Neck width: Seam to seam</Typography>
+    <div class="border-t my-5"></div>
+    <div class="space-y-4 flex-col text-sm">
+        {#each columns as column}
+            <div class="flex justify-between border-b pb-4">
+                <div>{column.label}</div>
+                <div>{column.value}</div>
+            </div>
+        {/each}
+    </div>
+</Drawer>
