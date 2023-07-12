754a060fe5f1515041bdc5dca4b70c593471778d
James Birtles <James.Birtles@peopleplus.co.uk>
2023-07-12 16:58:15 +0100

    fix up playwright tests

--- src/routes/test/carousel/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import { Carousel } from '$lib';
-
-	const items = Array.from({ length: 10 }).map((_, i) => `/carousel/${i}.jpg`);
-</script>
-
-<Carousel class="gap-2 px-2" {items} let:item>
-	<img src={item} width="400" height="400" alt="" class="w-[200px] rounded" />
-</Carousel>
