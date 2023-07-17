c7dd8e2bff77be40f42f55f0e5f83846f7aa774d
James Birtles <james@birtles.dev>
2023-07-17 15:53:19 +0100

    change some playwright options for CI (#661)

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,11 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	testDir: './playwright_tests',
 	testMatch: /.*.spec.ts/,
+	forbidOnly: !!process.env.CI,
+	workers: process.env.CI ? 1 : '75%',
+	retries: 3,
+	reporter: process.env.CI ? [['html'], ['github']] : undefined,
+	fullyParallel: true,
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173,
