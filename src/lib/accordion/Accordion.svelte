46257a12bbcd1d8eb6b645091152938c148f556a
Anna Fatani <79071385+annafatani@users.noreply.github.com>
2023-08-09 12:11:43 +0100

    Add tailwind merge in accordion (#772)
    * Add twMerge on label and content classes
    
    * Fix padding
    
    * Fix typo
    
    * remove span class
    
    * re-added focus styling

--- src/lib/accordion/Accordion.svelte
+++ src/lib/accordion/Accordion.svelte
@@ -6,6 +6,8 @@
 
 	let className = '';
 	export { className as class };
+	export let contentClass = '';
+	export let labelClass = '';
 	export let label: string;
 	export let use: UseActions = [];
 
@@ -35,7 +37,7 @@
 			class={twMerge(
 				'relative flex w-full items-center justify-between p-5 text-left font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-2 focus:ring-red-400 dark:text-gray-100 dark:hover:bg-gray-600',
 				group != null ? 'group-first:rounded-t-xl' : 'rounded-t-xl',
-				!$disclosure.expanded && (group != null ? 'group-last:rounded-b-xl' : 'rounded-b-xl'),
+				!$disclosure.expanded && (group != null ? 'group-last:rounded-b-xl' : 'rounded-b-xl'), labelClass
 			)}
 			use:disclosure.button
 		>
@@ -57,7 +59,7 @@
 	</h2>
 	<div
 		class:hidden={!$disclosure.expanded}
-		class="border-t border-gray-200 p-5"
+		class={twMerge('border-t border-gray-200 p-5', contentClass)}
 		use:disclosure.panel
 	>
 		<slot />
--- src/routes/(docs)/Accordion/ExampleAccordion.svelte
+++ src/routes/(docs)/Accordion/ExampleAccordion.svelte
@@ -1,12 +1,31 @@
 <script>
-	import { Accordion, AccordionGroup } from '$lib';
+	import {Accordion, AccordionGroup, Card} from '$lib';
 </script>
+
+<Card class="mb-10 w-[400px]">
+	<div class="rounded-t-xl h-36 bg-cover bg-center"
+		 style="background-image: url('/jumbotron.jpg');" />
+	<div class="p-3">
+		<strong>Alexander Bryne</strong>
+		<p class="italic text-sm text-gray-500">Project Manager</p>
+	</div>
+
+	<AccordionGroup exclusive>
+		<Accordion label="Learn More"
+				   labelClass="px-3 py-3 hover:bg-transparent text-sm"
+				   contentClass="p-0 border-t-0"
+				   class="bg-white border-none p-0">
+			<p class="text-gray-500 bg-gray-100 p-2 rounded-b-lg text-sm">
+				Alexander is an experienced project manager with vast experience in overseeing diverse teams. He excels in strategic planning, risk management, and is highly skilled in nurturing strong client relationships.
+			</p>
+		</Accordion>
+	</AccordionGroup>
+</Card>
 
 <AccordionGroup exclusive>
 	<Accordion label="Accordion 1">
 		<p class="text-gray-500">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id massa et metus blandit
-			malesuada.
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id massa et metus blandit malesuada.
 		</p>
 	</Accordion>
 	<Accordion label="Accordion 2">
