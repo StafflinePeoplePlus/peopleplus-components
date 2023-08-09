dfffc84b696bdf2eda6d2fbfb5b51cfa363e7865
Danny Lewis <danny.lewis@peopleplus.co.uk>
2023-08-09 12:19:33 +0100

    Lint fixes

--- src/lib/accordion/Accordion.svelte
+++ src/lib/accordion/Accordion.svelte
@@ -37,7 +37,8 @@
 			class={twMerge(
 				'relative flex w-full items-center justify-between p-5 text-left font-medium text-gray-500 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-2 focus:ring-red-400 dark:text-gray-100 dark:hover:bg-gray-600',
 				group != null ? 'group-first:rounded-t-xl' : 'rounded-t-xl',
-				!$disclosure.expanded && (group != null ? 'group-last:rounded-b-xl' : 'rounded-b-xl'), labelClass
+				!$disclosure.expanded && (group != null ? 'group-last:rounded-b-xl' : 'rounded-b-xl'),
+				labelClass,
 			)}
 			use:disclosure.button
 		>
--- src/routes/(docs)/Accordion/ExampleAccordion.svelte
+++ src/routes/(docs)/Accordion/ExampleAccordion.svelte
@@ -1,22 +1,28 @@
 <script>
-	import {Accordion, AccordionGroup, Card} from '$lib';
+	import { Accordion, AccordionGroup, Card } from '$lib';
 </script>
 
 <Card class="mb-10 w-[400px]">
-	<div class="rounded-t-xl h-36 bg-cover bg-center"
-		 style="background-image: url('/jumbotron.jpg');" />
+	<div
+		class="rounded-t-xl h-36 bg-cover bg-center"
+		style="background-image: url('/jumbotron.jpg');"
+	/>
 	<div class="p-3">
 		<strong>Alexander Bryne</strong>
 		<p class="italic text-sm text-gray-500">Project Manager</p>
 	</div>
 
 	<AccordionGroup exclusive>
-		<Accordion label="Learn More"
-				   labelClass="px-3 py-3 hover:bg-transparent text-sm"
-				   contentClass="p-0 border-t-0"
-				   class="bg-white border-none p-0">
+		<Accordion
+			label="Learn More"
+			labelClass="px-3 py-3 hover:bg-transparent text-sm"
+			contentClass="p-0 border-t-0"
+			class="bg-white border-none p-0"
+		>
 			<p class="text-gray-500 bg-gray-100 p-2 rounded-b-lg text-sm">
-				Alexander is an experienced project manager with vast experience in overseeing diverse teams. He excels in strategic planning, risk management, and is highly skilled in nurturing strong client relationships.
+				Alexander is an experienced project manager with vast experience in overseeing diverse
+				teams. He excels in strategic planning, risk management, and is highly skilled in nurturing
+				strong client relationships.
 			</p>
 		</Accordion>
 	</AccordionGroup>
@@ -25,7 +31,8 @@
 <AccordionGroup exclusive>
 	<Accordion label="Accordion 1">
 		<p class="text-gray-500">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id massa et metus blandit malesuada.
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id massa et metus blandit
+			malesuada.
 		</p>
 	</Accordion>
 	<Accordion label="Accordion 2">
