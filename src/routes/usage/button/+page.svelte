21223a8b14fe5bd5fa08f12a67f28c1a7bd22cde
James Birtles <james@birtles.dev>
2023-09-11 09:13:50 +0100

    add icon button and tweak button styling (#897)

--- src/routes/usage/button/+page.svelte
+++ src/routes/usage/button/+page.svelte
@@ -2,12 +2,8 @@
 	import { Button } from '$lib';
 </script>
 
-<div class="flex gap-2">
+<div class="flex items-center gap-2">
 	<!-- START USAGE -->
-	<Button>
-		<span aria-hidden="true">&LeftArrow;</span>
-		<span class="sr-only">Back</span>
-	</Button>
 	<Button>Get started &RightArrow;</Button>
 	<Button variant="secondary" href="https://peopleplus.co.uk">Learn more</Button>
 	<!-- END USAGE -->
