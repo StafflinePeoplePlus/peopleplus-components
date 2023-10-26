29e0cd76293476c1567e5a1549a0b61026254ab3
James Birtles <James.Birtles@peopleplus.co.uk>
2023-10-26 10:27:23 +0100

    add sidebar component

---
+++ src/routes/usage/sidebar/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Avatar, Sidebar, SidebarItem } from '$lib';
+	import {
+		BellIcon,
+		HomeIcon,
+		ListTodo,
+		LogOutIcon,
+		MessagesSquareIcon,
+		SettingsIcon,
+	} from 'lucide-svelte';
+</script>
+
+<div class="h-[600px] overflow-clip rounded-xl border bg-gray-100 shadow-md">
+	<!-- START USAGE -->
+	<Sidebar>
+		<img src="/peopleplus-logo.svg" class="mb-10 mt-4 h-10" alt="PeoplePlus Logo" />
+		<nav class="contents">
+			<ul class="flex grow flex-col gap-1">
+				<SidebarItem href="#" active><HomeIcon aria-hidden="true" /> Home</SidebarItem>
+				<SidebarItem href="#"><BellIcon aria-hidden="true" /> Notifications</SidebarItem>
+				<SidebarItem href="#"><MessagesSquareIcon aria-hidden="true" /> Messages</SidebarItem>
+				<SidebarItem href="#"><ListTodo aria-hidden="true" /> Tasks</SidebarItem>
+				<SidebarItem href="#"><SettingsIcon aria-hidden="true" /> Settings</SidebarItem>
+
+				<SidebarItem href="#" class="mt-auto">
+					<Avatar
+						aria-hidden="true"
+						size="xs"
+						name="Rich Harris"
+						src="https://avatars.githubusercontent.com/u/1162160?v=4"
+					/> Profile
+				</SidebarItem>
+				<SidebarItem><LogOutIcon aria-hidden="true" /> Log Out</SidebarItem>
+			</ul>
+		</nav>
+	</Sidebar>
+	<!-- END USAGE -->
+</div>
