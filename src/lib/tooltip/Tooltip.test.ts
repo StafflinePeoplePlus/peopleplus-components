ce1f5a532f1e1dacd0b958a42efc224312460960
Anna Fatani <anna.fatani@peopleplus.co.uk>
2023-12-12 12:17:53 +0000

    fix lint

--- src/lib/tooltip/Tooltip.test.ts
+++ src/lib/tooltip/Tooltip.test.ts
@@ -5,12 +5,12 @@ import { cleanup, render, fireEvent } from '@testing-library/svelte';
 afterEach(cleanup);
 
 test('should render tooltip content when hovered', async () => {
-	const { getByText, getByTestId } = render(TestTooltip);
+	const { getByText } = render(TestTooltip);
 
-	const tooltipTrigger = getByText('Tooltip Component');
+	const tooltipTrigger = getByText('Component');
 
-	await fireEvent.mouseOver(tooltipTrigger);
-	await new Promise((r) => setTimeout(r, 1000));
-	const tooltipContent = getByTestId('content');
+	await fireEvent.focus(tooltipTrigger);
+	await new Promise((r) => setTimeout(r, 500));
+	const tooltipContent = getByText('Content');
 	expect(tooltipContent).toBeVisible();
 });
