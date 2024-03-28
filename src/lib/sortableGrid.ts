569c7373709ad9302de579b2936f5bab45a67833
James Birtles <james@birtles.dev>
2024-03-28 13:44:48 +0000

    add support for drag handles on sortable grid

--- src/lib/sortableGrid.ts
+++ src/lib/sortableGrid.ts
@@ -28,11 +28,15 @@ export type SortableGridOpts = {
 
 export type SortableGrid = Readable<{ enabled: boolean; dragging: boolean }> & {
 	item: Action<HTMLElement, string>;
+	handle: Action<HTMLElement, string>;
+	target: Action<HTMLElement, string>;
+	image: Action<HTMLElement, string>;
 };
 export function createSortableGrid(opts: SortableGridOpts = {}): SortableGrid {
 	const enabled = opts.enabled ?? readable(true);
 	const dragging = writable(false);
 	let draggingId: string | undefined = undefined;
+	const dragImages = new Map<string, WeakRef<HTMLElement>>();
 
 	return {
 		...derived([enabled, dragging], ([enabled, dragging]) => ({
@@ -95,6 +99,102 @@ export function createSortableGrid(opts: SortableGridOpts = {}): SortableGrid {
 
 			return {
 				destroy,
+			};
+		}),
+		handle: toggleableAction<HTMLElement, string>(enabled, (el, id) => {
+			el.setAttribute('draggable', 'true');
+
+			const destroy = destroySequence(
+				() => {
+					el.removeAttribute('draggable');
+					delete el.dataset.dragging;
+				},
+				addEventListener(el, 'dragstart', (evt) => {
+					// Allow nested sortables to take precedence over their parents
+					evt.stopPropagation();
+					el.dataset.dragging = 'true';
+					draggingId = id;
+					dragging.set(true);
+
+					const dragImage = dragImages.get(id)?.deref();
+					if (dragImage) {
+						const imageRect = dragImage.getBoundingClientRect();
+						evt.dataTransfer?.setDragImage(
+							dragImage,
+							evt.clientX - imageRect.x,
+							evt.clientY - imageRect.y,
+						);
+						// Ensure any changes to the drag image element are applied after the drag
+						// image is set
+						requestAnimationFrame(() => {
+							dragImage.dataset.dragging = 'true';
+						});
+					}
+				}),
+				addEventListener(el, 'dragend', () => {
+					delete el.dataset.dragging;
+					draggingId = undefined;
+					dragging.set(false);
+					const dragImage = dragImages.get(id)?.deref();
+					if (dragImage) {
+						delete dragImage.dataset.dragging;
+					}
+				}),
+			);
+
+			return {
+				destroy,
+			};
+		}),
+		target: toggleableAction<HTMLElement, string>(enabled, (el, id) => {
+			let level = -1;
+			const destroy = destroySequence(
+				() => {
+					delete el.dataset.draggingOver;
+				},
+				addEventListener(el, 'dragover', (evt) => {
+					if (draggingId && draggingId !== id) {
+						evt.preventDefault();
+					}
+				}),
+				addEventListener(el, 'dragenter', () => {
+					if (draggingId && draggingId != id) {
+						level++;
+						if (level === 0) {
+							el.dataset.draggingOver = 'true';
+						}
+					}
+				}),
+				addEventListener(el, 'dragleave', () => {
+					if (draggingId && draggingId != id) {
+						level--;
+						if (level === -1) {
+							delete el.dataset.draggingOver;
+						}
+					}
+				}),
+				addEventListener(el, 'drop', () => {
+					if (draggingId) {
+						level = -1;
+						delete el.dataset.draggingOver;
+						opts.onReorder?.({
+							id: draggingId,
+							ontoId: id,
+						});
+					}
+				}),
+			);
+
+			return {
+				destroy,
+			};
+		}),
+		image: toggleableAction<HTMLElement, string>(enabled, (el, id) => {
+			dragImages.set(id, new WeakRef(el));
+			return {
+				destroy() {
+					dragImages.delete(id);
+				},
 			};
 		}),
 	};
